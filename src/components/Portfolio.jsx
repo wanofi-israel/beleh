b2109986ac8200c1ded95d78a9f43c721238c4ba
Wanofi Israel <wanofi@qualabels.local>
2025-08-18 16:39:04 +0300

    section portfolio almost complete

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Portfolio() {
+  return (
+    <div className='portfolio'>
+      <div className="portfolio-top">
+        <div className="portfolio-top_left">
+            
+        </div>
+        <div className="portfolio-top_right">
+            <div className="projects">
+                <h2>TELE BIRR</h2>
+            </div>
+            <div className="projects">
+                <h2>TELE BIRR</h2>
+            </div>
+            <div className="projects">
+                <h2>TELE BIRR</h2>
+            </div>
+            <div className="projects">
+                <h2>TELE BIRR</h2>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Portfolio
