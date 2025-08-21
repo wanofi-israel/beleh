62fc7be11ab548e10b1ab8ac3d67355738c5e345
mikiyasts <mikiyas.tse@gmail.com>
2025-08-21 12:10:27 +0300

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import ServiceCard from './components/ServiceCard'
-import Footer from './components/Footer'
-import ContactUs from './components/ContactUs'
-import { SplitText,ScrollTrigger } from 'gsap/all'
-import gsap from 'gsap'
-import Services from './components/Services'
-import Values from './components/Values'
-import ValuesDetail from './components/ValuesDetail'
-import Portfolio from './components/Portfolio'
-import Clients from './components/Clients'
-import { useGSAP } from '@gsap/react'
-import ContactForm from './components/ContactForm'
-=======
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
@@ -29,7 +12,7 @@ import ValuesDetail from "./components/ValuesDetail";
 import Portfolio from "./components/Portfolio";
 import Clients from "./components/Clients";
 import { useGSAP } from "@gsap/react";
->>>>>>> e3a22d0c121471a2244d30ed18e4a8bd92986859
+import ContactForm from "./components/ContactForm";
 function App() {
   gsap.registerPlugin(SplitText, ScrollTrigger);
 
@@ -55,19 +38,6 @@ function App() {
     });
   }, []);
   return (
-<<<<<<< HEAD
-    <div className='app'>
-    <Navbar/>
-    <Hero/>
-    <Services/>
-    <Values/>
-    <ValuesDetail/>
-    <Clients/>
-    <Portfolio/>
-<ContactUs/>
-<ContactForm/>
-    <Footer/>
-=======
     <div className="app">
       <Navbar />
       <Hero />
@@ -77,8 +47,7 @@ function App() {
       <Clients />
       <Portfolio />
       <ContactUs />
-      <Footer />
->>>>>>> e3a22d0c121471a2244d30ed18e4a8bd92986859
+      <ContactForm/>
     </div>
   );
 }
