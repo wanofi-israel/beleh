22f243c163bc16e282439c018d9cd8330e73f9eb
Wanofi Israel <wanofiisrael17@gmail.com>
2025-08-26 07:08:04 +0300

    link hover animation in progress rather than gradient try child div width scaling

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
-import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
+import React, { useRef } from 'react'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 function ContactUs() {
+  const linkRef=useRef(null)
   return (
     <div className='contactus-section'>
       <div>
-      <h1>Learn more about <br />our offerings</h1>
+        <h1>Learn more about <br />our offerings</h1>
       </div>
       <div>
-      <a href=""><ArrowOutwardIcon className='icon'/>Get intouch</a>
+        <div
+          className="link"
+          onMouseEnter={() => {
+            if (linkRef.current) {
+              // Reset animation so it can play again
+              linkRef.current.style.animation = 'none';
+              // Force reflow to restart animation
+              void linkRef.current.offsetWidth;
+              linkRef.current.style.animation = 'borderAnimation .5s ease-in-out forwards';
+            }
+          }}
+          onMouseLeave={() => {
+            if (linkRef.current) {
+              // Reset animation so it can play again
+              linkRef.current.style.animation = 'none';
+              // Force reflow to restart animation
+              void linkRef.current.offsetWidth;
+              linkRef.current.style.animation = 'borderAnimation .5s ease-in-out reverse';
+            }
+          }}
+        >
+          <div className="after" ref={linkRef}></div>
+          <a href="">
+            <div className="icon">
+              <div className="slide-icons">
+                <ArrowForwardIcon sx={{ fontSize: "1em", width: "100%", fill: "green" }} />
+                <ArrowForwardIcon sx={{ fontSize: "1em", width: "100%" }} />
+              </div>
+            </div>
+            Get intouch
+          </a>
+        </div>
       </div>
     </div>
   )
