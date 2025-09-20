f942c71d4c0da0c7eb79bcea59e8e6f178810a3a
Wanofi Israel <wanofi@qualabels.local>
2025-09-20 09:45:45 +0300

    fixed white logo when back to landing page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,19 @@
 import { ArrowBack } from '@mui/icons-material'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
 
 function NotFound() {
+
+  useGSAP(()=>{
+    gsap.to('.logoWhite',{
+      display:"none",
+    },0)
+    gsap.to('.logoDark',{
+      display:"flex",
+    },0)
+  },[])
   return (
     <div className='notfound-page'>
       <h1>404</h1>
